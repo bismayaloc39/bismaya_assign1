0e140c66266f3e774947ccb013e32d82192ab797
bismayaloc39 <126566609+bismayaloc39@users.noreply.github.com>
2023-03-12 03:18:57 +0530

    Add files via upload

---
+++ question2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+    int i=0;
+    printf("enter a number");
+    scanf("%d",&i);
+    if(i>0)
+    printf("%d is a positive number",i);
+    else if(i<0)
+    printf("%d is a negative number",i);
+    else 
+    if(i==0)
+    printf("%d is zero",i);
+}
+
